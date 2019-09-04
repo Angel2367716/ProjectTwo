05eb6c23b0d6ee4639bfcf42ceb30e775440ee53
Diego Matthews <diegomatthews9@gmail.com>
2019-09-03 18:13:19 -0700

    see if heroku works??

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
 app.use(express.static(__dirname + '/client/'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.port || 3000;
 server.listen(PORT);
 console.log('Server started. Port = ' + PORT);
 
